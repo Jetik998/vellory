2ead1719804144c82a947fcb8b0cce5a6addd825
jet <l-lellboy@yandex.ru>
2025-12-09 12:37:58 +0400

    Подключил Алембик

--- app/core/database.py
+++ app/core/database.py
@@ -12,7 +12,7 @@ async_session = async_sessionmaker(engine, expire_on_commit=False)
 # Функция создания всех таблиц
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
